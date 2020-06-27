787f228a90b5145bfddb676632978409fd515d52
jevvilla <juan.es-19@hotmail.com>
2020-06-27 14:10:11 -0500

    database connection stablished and app listening port added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import 'reflect-metadata';
+import { startServer } from './app';
 
+import { connect } from './config/typeorm';
+
+async function main() {
+  connect();
+  const app = await startServer();
+  app.listen(3000, (e) => {
+    console.log('server on port: 3000');
+  });
+}
+
+main();
